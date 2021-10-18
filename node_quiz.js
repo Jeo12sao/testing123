3fb7a74bb02551c4bd669257697a525887d3f1a3
Jeo12sao <88466724+Jeo12sao@users.noreply.github.com>
2021-10-18 16:18:34 -0400

    Add files via upload

---
+++ node_quiz.js
@@ -0,0 +1,198 @@
+node_quiz.js
+
+//Create and open a file in VS code and provide answers for the following questions//.
+//This is open book. I want you to go look it up if you don’t know.
+//Once you’ve completed it, push it up to a private repo on GitHub.
+
+//1.What is an IP Address? 
+//  Your IP address is your unique internet address similar to your home address. It tells websites, online
+//  services and other devices where to find you.  IP stands for "Internet Protocol," which is the set of
+//   rules governing the format of data sent via the internet or local network. The internet needs a way to 
+//  differentiate between different computers, routers, and websites
+ 
+//2. What is the IP address to your computer?
+//  192.168.14.29
+
+// 3. What is another name for the IP address of your computer?
+// Identifier,IPv4, IPv6,Public and Private IP Address,Local Host IP,Static and Dynamic IP, 
+// Broadcast and Multicast IP,Default Gateway IP
+
+//4. What is a Port?
+//   A computer port is a connection point or interface between a computer and an external or internal device.
+//   Internal ports may connect such devices as hard drives and CD ROM or DVD drives; external
+//   ports may connect modems, printers, mice and other devices.
+
+//  5.  What is a Domain Name and what’s happening when you make a request to it via your browser?
+//    A domain name is your website name. A domain name is the address where Internet users can access your website. 
+//      A domain name is used for finding and identifying computers on the Internet
+//    
+//    Computers use IP addresses, which are a series of number. However, it is difficult for humans to remember strings of numbers. Because of this,
+//    domain names were developed and used to identify entities on the Internet rather than using IP addresses.
+//    request:
+//     You enter a URL into a web browser
+//     The browser looks up the IP address for the domain name via DNS
+//     The browser sends a HTTP request to the server
+//     The server sends back a HTTP response
+//     The browser begins rendering the HTML
+//     The browser sends requests for additional objects embedded in HTML (images, css, JavaScript) and repeats steps 3-5.
+//     Once the page is loaded, the browser sends further async requests as needed.
+
+//6. What is a Server?
+//A server is a computer or system that provides resources, data, services, or programs to other computers, known as clients, over a network.
+//  In theory, whenever computers share resources with client machines they are considered servers. 
+//There are many types of servers, including web servers, mail servers, and virtual servers.
+
+// 7.Is a Server a part of the Backend or Frontend of the web?
+// Backend
+
+//8. What is a Client?
+//  A client is the receiving end of a service or the requestor of a service in a client/server model type of system.
+//  The client is most often located on another system or computer, which can be accessed via a network
+
+//9.Is a Client a part of the Backend or Frontend of the web?
+//  Frontend 
+//  Provide an example of a client.
+//   Chrome, Safari and Duck Duck GO
+
+//10. A Server-Side programming language is something that can do what?
+//server-side website programming mostly involves choosing which content is returned to the browser in response to requests.
+// The server-side code handles tasks like validating submitted data and requests, 
+// using databases to store and retrieve data and sending the correct data to the client as required
+
+//11.What are the primary languages to develop for the web / Client-Side?
+//HTML,CSS,JAVA,JAVASCRIPT,Python, SQL, PHP, NET, C, C++,Go
+//client side  JavaScript, HTML, and CSS.
+
+//12. What does Node allow us to do with one of the Client-Side languages?
+//Node is an engine that runs JavaScript on a server. The creators of Node took the JavaScript engine from 
+//browsers and added functionality so you can create backend applications using the JavaScript language.
+
+ 
+//13. What is one way to retrieve and use others code in my project?
+// API
+
+//14 Where can I find these different pieces of code?
+//ELEMENTS/npm
+
+//15. What are these pieces of code called?
+//modules/packages
+
+
+//16.How can I create my own piece of code for use in my project or others?
+//React-Router
+
+//17.How do I add that piece of code in a specific file I want to use it in?
+//React-Router
+
+//18. Where can I find all of the pieces of code I’ve added from others into my project?
+//console/elements
+
+//19.What is an HTTP request?
+//HTTP requests work as the intermediary transportation method between a client/application and a server. 
+//The client submits an HTTP request to the server, and after internalizing the message, the server sends back a response. 
+//The response contains status information about the request.
+
+//20. What are the various HTTP status codes and what do each of them relate to?
+//Informational responses (100–199)
+//Successful responses (200–299)
+//Redirection messages (300–399)
+//Client error responses (400–499)
+//Server error responses (500–599)
+
+//21.What is Routing?
+//Network routing is the process of selecting a path across one or more networks. The principles of routing can apply to any type of network, from telephone networks to public transportation. In packet-switching networks, such as the Internet, routing selects the paths for Internet Protocol (IP) packets to travel from their origin to their destination. These Internet routing
+// decisions are made by specialized pieces of network hardware called routers.
+
+//22.What is an API?
+//Application Programming Interface) API is the acronym for Application Programming Interface, which is a software intermediary that allows two applications to talk to each other. Each time you use an app like Facebook, send an instant message, 
+//or check the weather on your phone, you're using an API.
+
+//23. What is an API Key?
+//An application programming interface key (API key) is a unique piece of code that's sent from one application to another on a network. This is an essential part of digital security.
+// It prevents unauthorized programs from accessing a given function.
+
+//What is JSON?
+//JSON stands for J ava S cript O bject N otation. JSON is a lightweight format for storing and transporting data. JSON is often used when data is sent from a server to a web page.
+// JSON is "self-describing" and easy to understand
+
+//.25 What is the DOM?
+//DOM is an acronym that stands for Document Object Model. It's how a web browser represents a web page internally. 
+//The DOM determines what content should be on a page and how each element of the content relates to the other elements.
+//  The DOM represents the document as nodes and objects. That way, programming 
+// languages can connect to the page.
+// The DOM is the primary way that your JavaScript code will interact with a web page. Specifically, the browser provides a document 
+// variable that has properties and methods you can use to make changes to the page.
+//The  (DOM) is a set of utilities for manipulating XML documents (and, by extension, also HTML documents, which is what we are most likely interested in).
+//Before being able to make use of these utilities and helpers, DOM transforms XML files into a tree structure, that is,
+// a hierarchy of nodes. This tree represents both the content of the document and the relationship between the nodes.
+
+//.26 What is the BOM?
+//The Browser Object Model (BOM) allows JavaScript to talk to the browser 
+//about matters other than the content of the page.
+ 
+
+//27.What can we use in order to respond to some interaction on a web page?
+//div class,Buttons,greetings,etc
+
+//28. Referring to the previous question, what are some specific things we can respond to?
+//     greetings,password, name, etc
+
+//29.Where can I go to see a web pages’ requests and their statuses?
+//   network
+//30.What can I use in code to ensure a website looks good on both a desktop browser and mobile browser?
+//   performance/console
+//31.What can I use in a browser to ensure a website looks good on mobile and desktop?
+//   console/performance
+//32.  What type of CSS selector can I use to target a single element?
+//URLs with an # followed by an anchor name link to a certain element within a document. 
+//The element being linked to is the target element.
+
+//33.What type of CSS selector can I use to target multiple elements?
+//If you want to style all the headers in that class, you have to do it like this (which could also be done without the line breaks). 
+//Notice each selector has .live_grid in it:
+
+//.live_grid h1,
+//.live_grid h2,
+//.live_grid h3,
+//.live_grid h4,
+//.live_grid h5,
+//.live_grid h6 {
+    /* style here */
+//}
+//34.Describe the parent element and a child element.
+//ML Tree Structure. XML documents are formed as element trees. An XML tree starts at a root element and branches from the root to child elements. The terms parent, child, and sibling are used to describe the relationships between elements. Parents have children. Children have parents. 
+//Siblings are children on the same level (brothers and sisters).
+
+//35. What does the return keyword do in JavaScript?
+//The return keyword explained. Let's explore what the return keyword in JavaScript does. The return keyword in 
+//JavaScript is a keyword to build a statement that ends JavaScript code execution
+
+//36.What’s the difference between let and const?
+//The JavaScript let and const keywords provide block-level scope, but there is a slight difference in how they behave. 
+//With const, you can not re-assign a value to the variable. With let,
+//you can.
+
+//37.What are Promises in detail?
+//provides alternative to using callback for writing asynchronous code
+//represents a value that will eventually arrive
+//is an object with a .then() method and a .catch() method
+//and it has a status property with one of the following values:
+//pending
+//fulfilled
+//rejected
+
+//38.Why do we need Promises?
+//Use Promises to write cleaner, more readable asynchronous code.
+//Use the browser's built-in fetch() function to retrieve data from a remote API.
+//Build your asynchronous code as pipelines of pure functions, a.k.a. Promise chains.
+//Wrap synchronous APIs in Promises to use in Promise chains.
+//Coordinate multiple, interdependent Promise chains.
+//Handle exceptions thrown inside of your functions.
+//Use Promise.all() to wait for multiple Promises to finish.
+
+//39.What are two ways we can choose a set of code to run based on some condition.
+//We can use If and Esleif, 
+
+//40.What’s one thing I can use to help me debug my code?
+//Console will help because the error is listed
+
